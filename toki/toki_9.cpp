a0854402a8788af08761d02df1759ad6094a8036
R-twitzy <ramadhanmaulana446@gmail.com>
2023-09-15 08:33:05 +0700

    toki percabangan 3

---
+++ toki/toki_9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+
+    cin >> i;
+
+    if (i > 0)
+    {
+
+        cout << "positif";
+    }
+    else if (i < 0)
+    {
+        cout << "negatif";
+    }
+    else
+    {
+        cout << "nol";
+    }
+}
