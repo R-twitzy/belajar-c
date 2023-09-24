217a43ad06dc52ee7e684831525e0e11b4ba5488
R-twitzy <ramadhanmaulana446@gmail.com>
2023-09-24 10:26:06 +0700

    toki while pangkat

---
+++ toki/toki_16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    while (n%2 == 0)
+    {
+        n /= 2;
+    }
+
+    if (n==1)
+    {
+        cout << "ya" << endl;
+    } else
+    {
+        cout << "bukan" << endl;
+    }
+    
+    
+    
+}
