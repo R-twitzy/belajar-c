6990df24e7bf4856c1869b89317f775436d9ab90
R-twitzy <ramadhanmaulana446@gmail.com>
2023-10-09 21:10:47 +0700

    anjai praktek 3.3

---
+++ praktikum/148_prak3_3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int A, B, key, tmp, count = 0;
+
+    srand(time(NULL));
+
+    A = rand() %10 + 1;
+    B = rand() %100;
+    key = rand() %B + 1;
+
+    cout << "KELUARAN anda \t KELUARAN penguji" << endl;
+    cout << "\t \t " << A << ' ' << B << endl;
+
+    while (tmp != key && count < 15)
+    {
+        cin >> tmp;
+
+        if (tmp == key)
+        {
+            cout << "\t \t nah bener lu gw maapin" << endl;
+        }
+        else if (tmp < key)
+        {
+            cout << "\t \t terlalu kecil" << endl;
+        }
+        else if (tmp > key)
+        {
+            cout << "\t \t terlalu besar" << endl;
+        }
+
+        count++;
+    }
+    return 0;
+}
