47384eb901d6836a80d20a6fa890276ec321eea2
R-twitzy <ramadhanmaulana446@gmail.com>
2023-10-30 21:07:07 +0700

    anjai praktek 4.3

---
+++ praktikum/148_prak4_3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>   
+
+using namespace std;
+
+bool isAnagram(string x, string y){
+    /* NOTES: lower/uppercase berbeda tetap dianggap anagram*/
+
+    // lowercase kedua string
+    transform(x.begin(), x.end(), x.begin(), ::tolower);
+    transform(y.begin(), y.end(), y.begin(), ::tolower);
+
+    // mengurutkan kedua string
+    sort(x.begin(), x.end());
+    sort(y.begin(), y.end());
+
+    return x == y;
+}
+
+int main(){
+    string S1, S2;
+
+    getline(cin, S1);
+    getline(cin, S2);
+
+    if (isAnagram(S1, S2))
+    {
+        cout << "YA" << endl;
+    } else
+    {
+        cout << "BUKAN" << endl;
+    }
+    
+    
+
+    return 0;
+}
