45fedf489b6f1f8b370a2675b1e2941cb99146f8
R-twitzy <ramadhanmaulana446@gmail.com>
2023-11-13 18:32:48 +0700

    praktek 5.1

---
+++ praktikum/148_Prak5_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void printTengah(int arr[], int size){
+    int indexTengah = size/2;
+
+    cout << arr[indexTengah-1] << arr[indexTengah] << arr[indexTengah+1] << endl;
+}
+
+int main(){
+    int T;
+
+    cin >> T;
+
+    int A[T];
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A[i];
+    }
+    
+    printTengah(A, T);
+
+    return 0;
+}
