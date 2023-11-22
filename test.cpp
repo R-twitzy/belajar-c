80d7d6b9a7806b011205f91c349abd2715d722f7
R-twitzy <ramadhanmaulana446@gmail.com>
2023-11-22 11:28:47 +0700

    test

--- test.cpp
+++ test.cpp
@@ -4,19 +4,30 @@ using namespace std;
 
 int main()
 {
-  int n, tmp=2;
+  int value1 = 5, value2 = 15;
 
-  cout << "masukan tinggi segi3: ";
-  cin >> n;
+  int *p1, *p2;
 
-  for (int i =1; i <= n; i++)
-  {
-    for (int j = 0; j < i; j++)
-    {
-      cout << tmp << " ";
-      tmp+=2;
-    }
-    cout << endl;
-  }
-  
+  p1 = &value1; // p1 = alamat value1
+
+  p2 = &value2; // p2 = alamat value2
+
+  *p1 = 10; // nilai yang ditunjuk p1=10
+
+  *p2 = *p1; // nilai yang ditunjuk p2= nilai
+
+  // yang ditunjuk p1
+
+  p1 = p2;
+  // p1 = p2 (nilai pointer disalin)
+
+  *p1 = 20; // nilai yang ditunjuk p1 = 20
+
+  cout << "value1==" << value1 << "/value2==" << value2 << endl;
+
+  cout << "*P1==" << *p1 << "/ *P2==" << *p2 << endl;
+
+  cout << "P1==" << p1 << "/ P2==" << p2;
+
+  return 0;
 }
