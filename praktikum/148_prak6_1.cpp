39bddc64a549267b205b605ac1ba1033f246156e
R-twitzy <ramadhanmaulana446@gmail.com>
2023-11-27 19:05:23 +0700

    1 error

---
+++ praktikum/148_prak6_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string kata;
+    cin >> kata;
+
+    int vokal= 0;
+    char* tmp= kata.c_str();
+
+    while (*tmp != '\0') {
+    // Memeriksa apakah karakter saat ini adalah vokal
+    if (*tmp == 'a' || *tmp == 'i' || *tmp == 'u' || *tmp == 'e' || *tmp == 'o') {
+      // Menambahkan 1 ke jumlah vokal
+      vokal++;
+    }
+
+    // Majukan pointer ke karakter berikutnya
+    tmp++;
+  }
+
+    cout << vokal << endl;
+    return 0;
+}
